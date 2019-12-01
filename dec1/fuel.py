4aeb7954eb5b912ae10a7c71e6e4484f5a559a54
redshiftzero <jen@freedom.press>
2019-12-01 14:14:54 -0500

    dec 1: puzzle 2

--- dec1/fuel.py
+++ dec1/fuel.py
@@ -1,25 +1,52 @@
 import math
 
+from typing import List
+
+
+def compute_fuel(mass: int) -> int:
+    return math.floor(mass / 3) - 2
+
+
+class Launch():
+    def __init__(self, module_masses: List[int]):
+        self.modules = []
+        for mass in module_masses:
+            module = Module(mass)
+            self.modules.append(module)
+
+    def get_fuel(self) -> int:
+        total_fuel = 0
+        for module in self.modules:
+            total_fuel += module.get_fuel()
+        return total_fuel
+
 
 class Module():
     def __init__(self, mass: int):
         self.mass = mass
     
     def get_fuel(self) -> int:
-        fuel_required = math.floor(self.mass / 3) - 2
-        return fuel_required
+        fuel_to_compute = True
+        total_fuel = 0
+        mass = self.mass
+
+        while fuel_to_compute:
+            fuel_required = compute_fuel(mass)
+
+            if fuel_required <= 0:
+                fuel_to_compute = False
+            else:
+                total_fuel += fuel_required
+                mass = fuel_required  # For next iteration
+
+        return total_fuel
 
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         lines = f.readlines()
 
-    # test cases
-    # modules = [12, 14, 1969, 100756]
+    #modules = [12, 14, 1969, 100756]  # test cases
     modules = [int(x) for x in lines]
-
-    total_fuel = 0
-    for module in modules:
-        mod = Module(module)
-        total_fuel += mod.get_fuel()
-    print(total_fuel)
+    launch = Launch(modules)
+    print(launch.get_fuel())

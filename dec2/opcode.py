64e406910baeea77c8501389411cf524e670b60e
redshiftzero <jen@freedom.press>
2019-12-02 00:32:21 -0500

    Dec 2: puzzle 2

--- dec2/opcode.py
+++ dec2/opcode.py
@@ -18,10 +18,10 @@ class Computer():
     def __init__(self, opcodes: List[int]):
         self.opcodes = opcodes
         self.instruction_pointer = 0
+        self.keep_running = True
 
     def process(self) -> None:
-        while True:
-            #print('processing: {}'.format(self.instruction_pointer))
+        while self.keep_running:
             self._process_opcode(self.instruction_pointer)
             self.instruction_pointer += 4
 
@@ -41,12 +41,10 @@ class Computer():
         opcode = self.opcodes[index]
         try:
             operation = opcode_dict[opcode]
-            #print(operation)
             if operation == 'ADD' or operation == 'MULTIPLY':
                 self._arithmetic_opcode(index, operation)
             elif operation == 'STOP':
-                print(self.opcodes)
-                sys.exit(0)
+                self.keep_running = False
         except KeyError:
             raise OpcodeNotFound
 
@@ -57,8 +55,16 @@ if __name__=="__main__":
 
     opcodes = [int(x) for x in opcodes]
 
-    opcodes[1] = 12
-    opcodes[2] = 2
     #opcodes = [1,9,10,3,2,3,11,0,99,30,40,50]  # test case
-    computer = Computer(opcodes)
-    computer.process()
+    target_output = 19690720
+
+    # O(N*M) solution
+    for verb in range(1, 100):
+        for noun in range(1, 100):
+            test_opcodes = opcodes.copy()
+            test_opcodes[1] = noun
+            test_opcodes[2] = verb
+            computer = Computer(test_opcodes)
+            computer.process()
+            if computer.opcodes[0] == target_output:
+                print(100 * noun + verb)

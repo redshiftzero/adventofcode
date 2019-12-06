070b124aced7e2fbcedb83f0606c0816e0d86ce1
redshiftzero <jen@freedom.press>
2019-12-06 00:10:26 -0500

    Dec 5, puzzle 2

--- dec5/intcode_calc.py
+++ dec5/intcode_calc.py
@@ -7,12 +7,18 @@ class OpcodeNotFound(Exception):
     pass
 
 
-# Opcodes that have parameter mode support.
+OPCODE_BYTE_LEN = 2
+
+
 opcode_parameter_mode = {
     1: {'operation': 'ADD', 'nargs': 3},
     2: {'operation': 'MULTIPLY', 'nargs': 3},
     3: {'operation': 'INPUT', 'nargs': 1},
     4: {'operation': 'OUTPUT', 'nargs': 1},
+    5: {'operation': 'JUMP_IF_TRUE', 'nargs': 2},
+    6: {'operation': 'JUMP_IF_FALSE', 'nargs': 2},
+    7: {'operation': 'LESS_THAN', 'nargs': 3},
+    8: {'operation': 'EQUALS', 'nargs': 3},
 }
 
 opcode_no_parameter_mode = {
@@ -33,16 +39,16 @@ class Computer():
 
     def process(self) -> None:
         while self.keep_running:
-            self._process_opcode(self.instruction_pointer)
+            self._process_instruction(self.instruction_pointer)
 
-    def _process_opcode(self, index: int) -> None:
+    def _process_instruction(self, index: int) -> None:
         opcode = self.opcodes[index]
     
         try:
             op_lookup = opcode_no_parameter_mode[opcode]
         except KeyError:
             try:
-                opcode_str = str(opcode).zfill(5)
+                opcode_str = str(opcode)
                 matching_op = opcode_str[-2:]
                 op_lookup = opcode_parameter_mode[int(opcode_str[-1])]
             except KeyError:
@@ -50,6 +56,7 @@ class Computer():
 
         # Process no parameter mode instructions
         operation = op_lookup['operation']
+        opcode_str = str(opcode).zfill(OPCODE_BYTE_LEN + op_lookup['nargs'])
         if operation == 'STOP':
             self.keep_running = False
 
@@ -68,15 +75,81 @@ class Computer():
         elif operation == 'OUTPUT':
             self._output(index, modes)
             self.instruction_pointer += 2
+        elif operation == 'JUMP_IF_TRUE':
+            self._jump_if_true_opcode(index, modes)
+        elif operation == 'JUMP_IF_FALSE':
+            self._jump_if_false_opcode(index, modes)
+        elif operation == 'LESS_THAN':
+            self._less_than_opcode(index, modes)
+            self.instruction_pointer += 4
+        elif operation == 'EQUALS':
+            self._equals_opcode(index, modes)
+            self.instruction_pointer += 4
 
-    def _opcode_write(self, index: int, mode: str, value: int) -> None:
+    def _less_than_opcode(self, index: int, modes: List[str]) -> None:
+        """
+        if the first parameter is less than the second parameter, it
+        stores 1 in the position given by the third parameter. Otherwise,
+        it stores 0.
+        """
+        value_1 = self._read(index + 1, modes[0])
+        value_2 = self._read(index + 2, modes[1])
+
+        if value_1 < value_2:
+            result = 1
+        else:
+            result = 0
+
+        self._write(index + 3, modes[2], result)
+
+    def _equals_opcode(self, index: int, modes: List[str]) -> None:
+        """
+        if the first parameter is equal to the second parameter, it
+        stores 1 in the position given by the third parameter. Otherwise,
+        it stores 0.
+        """
+        value_1 = self._read(index + 1, modes[0])
+        value_2 = self._read(index + 2, modes[1])
+
+        if value_1 == value_2:
+            result = 1
+        else:
+            result = 0
+
+        self._write(index + 3, modes[2], result)
+
+    def _jump_if_true_opcode(self, index: int, modes: List[str]) -> None:
+        """
+        if the first parameter is non-zero, it sets the instruction pointer
+        to the value from the second parameter. Otherwise, it does nothing.
+        """
+        value_1 = self._read(index + 1, modes[0])
+        value_2 = self._read(index + 2, modes[1])
+        if value_1 != 0:
+            self.instruction_pointer = value_2
+        else:
+            self.instruction_pointer += 3
+
+    def _jump_if_false_opcode(self, index: int, modes: List[str]) -> None:
+        """
+        if the first parameter is zero, it sets the instruction pointer
+        to the value from the second parameter. Otherwise, it does nothing.
+        """
+        value_1 = self._read(index + 1, modes[0])
+        value_2 = self._read(index + 2, modes[1])
+        if value_1 == 0:
+            self.instruction_pointer = value_2
+        else:
+            self.instruction_pointer += 3
+
+    def _write(self, index: int, mode: str, value: int) -> None:
         if mode == 'POSITION':
             position = self.opcodes[index]
             self.opcodes[position] = value
         elif mode == 'IMMEDIATE':
             self.opcodes[index] = value
 
-    def _opcode_read(self, index: int, mode: str) -> int:
+    def _read(self, index: int, mode: str) -> int:
         if mode == 'POSITION':
             position = self.opcodes[index]
             value = self.opcodes[position]
@@ -86,22 +159,22 @@ class Computer():
 
     def _input(self, index: int, modes: List[str]) -> None:
         input_value = int(input("Program needs input (single integer plz): "))
-        self._opcode_write(index + 1, modes[0], input_value)
+        self._write(index + 1, modes[0], input_value)
 
     def _output(self, index: int, modes: List[str]) -> None:
-        value = self._opcode_read(index + 1, modes[0])
+        value = self._read(index + 1, modes[0])
         print("Program output: ", value)
 
     def _arithmetic_opcode(self, index: int, operation: str, modes: List[str]) -> None:
-        value_1 = self._opcode_read(index + 1, modes[0])
-        value_2 = self._opcode_read(index + 2, modes[1])
+        value_1 = self._read(index + 1, modes[0])
+        value_2 = self._read(index + 2, modes[1])
 
         if operation == 'ADD':
             result = value_1 + value_2
         elif operation == 'MULTIPLY':
             result = value_1 * value_2
 
-        self._opcode_write(index + 3, modes[2], result)
+        self._write(index + 3, modes[2], result)
 
 
 if __name__=="__main__":
@@ -110,7 +183,8 @@ if __name__=="__main__":
 
     opcodes = [int(x) for x in opcodes]
 
-    # opcodes = [3,0,4,0,99]  # test case
+    # test case is below
+    #opcodes = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99] # test case
 
     computer = Computer(opcodes)
     computer.process()

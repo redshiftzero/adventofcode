f486ac105c7f49adc12190e1360170fa27505fd4
redshiftzero <jen@freedom.press>
2019-12-06 18:55:26 -0500

    dec 6 Puzzle 2

--- dec6/orbits.py
+++ dec6/orbits.py
@@ -1,3 +1,5 @@
+import heapq
+
 from typing import Dict, List, Optional
 
 
@@ -5,9 +7,13 @@ class Orbit():
     def __init__(self, name: str):
         self.name = name
         self.children = []  # List[Orbit]
+        self.parents = []  # List[Orbit]
 
     def __repr__(self) -> str:
         return 'Orbit("{}")'.format(self.name)
+
+    def __lt__(self, other) -> str:
+        return other
 
     def __str__(self) -> str:
         if self.children:
@@ -17,6 +23,9 @@ class Orbit():
 
     def add_child(self, child) -> None:
         self.children.append(child)
+
+    def add_parent(self, parent) -> None:
+        self.parents.append(parent)
 
 
 class Universe():
@@ -37,6 +46,12 @@ class Universe():
             if child in orbit.children:
                 return orbit
 
+    def set_parents(self) -> List[Orbit]:
+        for x in list(self.orbits.values()):
+            for y in list(self.orbits.values()):
+                if x in y.children:
+                    x.add_parent(y)
+
     def count_orbits(self) -> int:
         # First exclude the root node (which orbits nothing)
         nodes_to_count = self.orbits.copy()
@@ -56,14 +71,43 @@ class Universe():
 
         return num_orbits
 
+    def shortest_path(self, start: Orbit, target: Orbit) -> int:
+        visited = set()
+        q = [[start]]
+
+        nodes_to_traverse = self.orbits.copy()
+        del nodes_to_traverse[start.name]
+
+        paths = []
+        while q:
+            path = q.pop(0)
+            node = path[-1]
+
+            if node == target:
+                paths.append(path)
+            elif node not in visited:
+                for neighbor in node.children + node.parents:
+                    new_path = list(path)
+                    new_path.append(neighbor)
+                    q.append(new_path)
+
+                visited.add(node)
+            print('visited: {}'.format(node))
+
+        # minus 2 for the start and end nodes
+        # minus 1 for the fact we want the "orbital transfers"
+        distances = [len(x) - 2 - 1 for x in paths]
+        print(paths)
+        return min(distances)
+
 
 if __name__ == "__main__":
     with open('input.txt', 'r') as f:
         input_ = f.readlines()
 
-    # test input
-    #input_ = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G',
-    #          'G)H', 'D)I', 'E)J', 'J)K', 'K)L']
+    #input_ = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F',
+    #          'B)G', 'G)H', 'D)I', 'E)J', 'J)K',
+    #          'K)L', 'K)YOU', 'I)SAN']
 
     universe = Universe()
     for line in input_:
@@ -81,5 +125,8 @@ if __name__ == "__main__":
             universe.add_orbit(parent_orbit)
         
         parent_orbit.add_child(child_orbit)
-    
-    print(universe.count_orbits())
+
+    universe.set_parents()
+    start = universe['YOU']
+    target = universe['SAN']
+    print(universe.shortest_path(start, target))
